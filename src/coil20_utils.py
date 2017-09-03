d2d672a41f9a1fcbe38d2e77cf82eb452e1b012a
lambdaofgod <bartczukkuba@gmail.com>
2017-09-03 23:39:40 +0200

    manifold learning and COIL20 dataset

--- src/coil20_utils.py
+++ src/coil20_utils.py
@@ -6,6 +6,7 @@
 import os
 import load_data_utils
 from skimage.io import imread_collection, concatenate_images
+import matplotlib.pyplot as plt
 
 
 def load_images():
@@ -22,10 +23,21 @@ def load_images():
     __, DATA_DIR = load_data_utils.get_env_vars()
     COIL20_DIR = DATA_DIR + '/' +  COIL20_SUFFIX
     
-    print(COIL20_DIR) 
     image_list =  imread_collection(COIL20_DIR + '/*.png')
     images = concatenate_images(image_list)
     imshape = images.shape
     reshaped_images = images.reshape(imshape[0], imshape[1] * imshape[2])
     return reshaped_images 
+
+
+def display_image(img, **kwds):
+    """
+    input:
+
+    img 
+    a 128x128 or 16384 ndarray
+    """
+    tmp_img = img.reshape(128, 128)
+    plt.imshow(tmp_img, **kwds)
+    plt.show()
 
