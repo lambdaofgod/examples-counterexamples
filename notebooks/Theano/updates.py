30bfdb15bfa371cb25b71559c48e36e4d12d850f
lambdaofgod <bartczukkuba@gmail.com>
2017-09-21 23:41:29 +0200

    added comments to python files

--- notebooks/Theano/updates.py
+++ notebooks/Theano/updates.py
@@ -1,8 +1,45 @@
+import numpy as np
+
+import theano
+import theano.tensor as T
+
+
 def gradient_descent_update(loss, weights, learning_rate):
+  """
+  Updates for gradient descent learning method
+  W_{t+1} = W_t - learning_rate * grad(W_t)
+
+  Parameters
+  ----------
+
+  loss : theano tensor, scalar function
+
+  weights : theano tensor, model parameters
+
+  learning_rate : scalar
+  """
   weights_gradient = T.grad(cost=loss, wrt=weights)
   return (weights, weights - learning_rate * weights_gradient)
 
+
 def momentum_method_updates(loss, weights, learning_rate, momentum):
+  """
+  Updates for momentum learning method
+  V_{t+1} = momentum * V_t - learning_rate * grad(W_t)
+  W_{t+1} = W_t + V_{t+1}
+  
+  
+  Parameters
+  ----------
+
+  loss : theano tensor, scalar function
+
+  weights : theano tensor, model parameters
+
+  learning_rate : scalar
+
+  momentum : scalar
+  """
   weights_gradient = T.grad(cost=loss, wrt=weights)
   velocity = theano.shared(
     value=np.zeros(
