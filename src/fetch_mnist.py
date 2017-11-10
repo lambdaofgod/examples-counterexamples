3d5640b9880316f5d31b1909d32624b09082df32
lambdaofgod <bartczukkuba@gmail.com>
2017-11-10 21:01:23 +0100

    better preprocessing utils

--- src/fetch_mnist.py
+++ src/fetch_mnist.py
@@ -1,9 +1,9 @@
 from sklearn.datasets import fetch_mldata
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
-def preprocessed_mnist(random_state):
+def preprocessed_mnist(random_state, preprocess='standardize'):
     """
     Fetches mnist dataset and then applies standard scaling
 
@@ -15,11 +15,35 @@ def preprocessed_mnist(random_state):
 
     X = mnist['data']
     y = mnist['target'].astype(int)
-    X_train_raw, X_test_raw, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y,
-                                                                random_state=random_state)
+    if preprocess == 'standardize':
+        return standardize(X, y, random_state)
+    elif preprocess == 'scale_to_unit_cube':
+        return scale_to_unit_cube(X, y, random_state)
+    else:
+        raise NotImplementedError('Unsupported preprocessing method: {}'.format(preprocess))
 
-    sscaler = StandardScaler()
-    X_train = sscaler.fit_transform(X_train_raw)
-    X_test = sscaler.transform(X_test_raw)
 
-    return (X_train, X_test, y_train, y_test)
+def scale_to_unit_cube(X_raw, y, random_state):
+    X_mean_subtracted = X_raw - X_raw.mean()
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+
+    X = scaler.fit_transform(X_mean_subtracted)
+    return (train_test_split(
+        X,
+        y,
+        test_size=0.2, stratify=y,
+        random_state=random_state))
+
+
+def standardize(X_raw, y, random_state):
+    scaler = StandardScaler()
+
+    X_train_raw, X_test_raw, y_train, y_test = train_test_split(
+        X_raw,
+        y,
+        test_size=0.2, stratify=y,
+        random_state=random_state)
+    X_train = scaler.fit_transform(X_train_raw)
+    X_test = scaler.transform(X_test_raw)
+
+    return X_train, X_test, y_train, y_test
