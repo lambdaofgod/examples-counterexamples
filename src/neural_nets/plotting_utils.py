926ad94dd5cd22922ae07d17990df26ed7c4190e
lambdaofgod <bartczukkuba@gmail.com>
2018-02-12 20:51:14 +0100

    refactored plotting code

--- src/neural_nets/plotting_utils.py
+++ src/neural_nets/plotting_utils.py
@@ -8,6 +8,7 @@ class Plotter:
         self.X_test = X_test
         self.y_test = y_test
 
+
     def plot_loss_and_accuracy(self, model, add_smoothing=True):
         plt.plot(model.losses)
 
@@ -16,21 +17,33 @@ class Plotter:
         plt.ylabel('Loss')
         print('Final loss: {:.4f}'.format(model.losses[-1]))
 
+        loss = model.losses
+
         if add_smoothing:
-            ma_width = 5
+            smoothed_loss = self._smooth_ma(loss)
+            plt.plot(smoothed_loss)
+            print('Final loss, smoothed: {:.4f}'.format(smoothed_loss[-1]))
+        else:
+            plt.plot(loss)
 
-            # pad the array so that numpy moving average makes sense for the whole range
-            pad_width = (ma_width // 2, ma_width // 2)
-            padded_losses = np.pad(model.losses, pad_width=pad_width, mode='edge')
-
-            smoothed_sgd_loss = np.convolve(
-                padded_losses,
-                v=np.ones(ma_width) / ma_width,
-                mode='valid')
-            plt.plot(smoothed_sgd_loss)
-            print('Final loss, smoothed: {:.4f}'.format(smoothed_sgd_loss[-1]))
         plt.show()
 
         y_pred = model.predict(self.X_test)
         print('accuracy: {:.4f}'.format(model.score(self.X_test, self.y_test)))
         print(classification_report(self.y_test, y_pred))
+
+
+    @staticmethod
+    def _smooth_ma(series, window_width=10):
+        """
+        Smooths input with moving average of window_width
+        """
+        pad_width = (window_width // 2, window_width // 2)
+
+        # pad the array so that numpy moving average makes sense for the whole range
+        padded_series = np.pad(series, pad_width=pad_width, mode='edge')
+
+        return np.convolve(
+            padded_series,
+            v=np.ones(window_width) / window_width,
+            mode='valid')
